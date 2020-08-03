2dae915f84beb4b50547cbdacb6762ec14d205ad
multipolygon <multipolygon@gmail.com>
2020-08-03 16:37:13 +1000

    update env config

--- next.config.js
+++ next.config.js
@@ -4,23 +4,22 @@ const nextConfig = {
         appShortName: process.env.APP_SHORT_NAME || 'NS',
         appHost: process.env.APP_HOST || 'https://natureshare.org.au',
 
+        apiHost: process.env.API_HOST,
+        passwordSalt: process.env.PASSWORD_SALT || 'ns1234',
+
         osmHost: process.env.OSM_HOST || 'a.tile.openstreetmap.org',
         tfApiKey: process.env.TF_API_KEY,
 
-        apiHost: process.env.API_HOST,
-
         contentHost: process.env.CONTENT_HOST || 'https://files.natureshare.org.au/',
-        contentHostDev: process.env.CONTENT_HOST_DEV,
-
         speciesHost: process.env.SPECIES_HOST || 'https://species.natureshare.org.au/',
-        speciesHostDev: process.env.SPECIES_HOST_DEV,
-
         externalHosts: process.env.EXTERNAL_HOSTS,
 
         githubContentPath: process.env.GH_CONTENT_PATH || 'natureshare/natureshare-files',
         githubSpeciesPath: process.env.GH_SPECIES_PATH || 'natureshare/natureshare-species-wiki',
 
-        passwordSalt: process.env.PASSWORD_SALT || 'ns1234',
+        // Dev:
+        contentHostDev: process.env.CONTENT_HOST_DEV,
+        speciesHostDev: process.env.SPECIES_HOST_DEV,
     },
     exportTrailingSlash: false,
 };
